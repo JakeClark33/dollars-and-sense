913e4f25b77b3d5b9fde29d48eabe345b520ae6c
JakeClark33 <jakemclark@hotmail.com>
2021-10-07 17:49:41 -0500

    done

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,64 @@
+const webpack = require('webpack');
 const path = require("path");
 
-module.exports = {
+const config = {
     entry: './public/js/index.js',
     output: {
             path: path.resolve(__dirname, 'dist'),
             filename: 'main.bundle.js'
         },
-    mode: 'development'
-};
+        module: {
+            rules: [
+              {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      esModule: false,
+                      name(file) {
+                        return '[path][name].[ext]';
+                      },
+                      publicPath(url) {
+                        return url.replace('../', '/public/');
+                      }
+                    }
+                  },
+                  {
+                    loader: 'image-webpack-loader'
+                  }
+                ]
+              }
+            ]
+          },
+          plugins: [
+            new webpack.ProvidePlugin({
+              $: 'jquery',
+              jQuery: 'jquery'
+            }),
+            new BundleAnalyzerPlugin({
+              analyzerMode: 'static'
+            }),
+            new WebpackPwaManifest({
+              name: 'Dollars And Sense',
+              short_name: 'Balance',
+              description: 'An app that allows you to view and update your budget.',
+              start_url: '../index.html',
+              background_color: '#01579b',
+              theme_color: '#ffffff',
+              fingerprints: false,
+              inject: false,
+              icons: [
+                {
+                  src: path.resolve('assets/img/icons/icon-512x512.png'),
+                  sizes: [96, 128, 192, 256, 384, 512],
+                  destination: path.join('assets', 'icons')
+                }
+              ]
+            })
+          ],
+          mode: 'development'
+        };
+        
+        module.exports = config;
+        
